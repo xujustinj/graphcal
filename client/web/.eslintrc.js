26a695bcbe7a52c2314c5c13b0a127b7dfaf61f6
Justin Xu <xu.justin.j@gmail.com>
2023-06-22 20:28:34 -0400

    Fix various template-related React issues
    Fixed:
    - JSX files need to explicitly import React
    - Prettier warnings showing up as code errors in Error Lens
    - using legacy React <=17 root API

--- client/web/.eslintrc.js
+++ client/web/.eslintrc.js
@@ -1,42 +1,43 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "react", "react-hooks", "eslint-plugin-import", "prettier"],
-    env: {
-        browser: true
-    },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:prettier/recommended"
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-hooks", "eslint-plugin-import"],
+  env: {
+    browser: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended"
+  ],
+  parserOptions: {
+    project: ["tsconfig.json"],
+    ecmaVersion: 2020,
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", {ignoreRestSiblings: true}],
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".jsx", ".tsx"]
+      }
     ],
-    parserOptions: {
-        project: ["tsconfig.json"],
-        ecmaVersion: 2020,
-        tsconfigRootDir: __dirname,
-        sourceType: "module",
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    rules: {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-unused-vars": ["warn", {ignoreRestSiblings: true}],
-        "react/jsx-filename-extension": [
-            "warn",
-            {
-                extensions: [".jsx", ".tsx"]
-            }
-        ],
-        "react/prop-types": "off",
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-        "no-shadow": ["error", {builtinGlobals: false, hoist: "functions", allow: []}]
-    },
-    settings: {
-        react: {
-            version: "detect"
-        }
-    },
-    ignorePatterns: [".eslintrc.js"] // see https://stackoverflow.com/a/75791515
+    "react/jsx-uses-react": "off",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "no-shadow": ["error", {builtinGlobals: false, hoist: "functions", allow: []}]
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  ignorePatterns: [".eslintrc.js"] // see https://stackoverflow.com/a/75791515
 };
